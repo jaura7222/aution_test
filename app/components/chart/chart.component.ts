8529971eaaec6d908b9e82ed1e94c504aa0bb39c
jaura7222 <jaura7222@gmail.com>
2017-11-22 18:10:20 +0900

    sss

--- app/components/chart/chart.component.ts
+++ app/components/chart/chart.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+// import * as _ from '../../../manual_module/bootstrap/vendor/datatables/jquery.dataTables.js';
+// import * as _ from '../../../manual_module/bootstrap/js/sb-admin-datatables.min.js';
+// import * as _ from '../../../manual_module/bootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js';
+// import * as _ from '../../../manual_module/bootstrap/vendor/jquery-easing/jquery.easing.min.js';
+// import * as _ from '../../../manual_module/bootstrap/js/sb-admin.min.js';
 
 @Component({
 	selector : 'chart-page',
-	templateUrl : 'app/components/chart/chart.component.html'
+    templateUrl : 'app/components/chart/chart.component.html',
+    encapsulation : ViewEncapsulation.None
 })
 export default class ChartComponent {
 	
-
+    ngOnInit(){
+        $("#dataTable").DataTable();
+    }
 }

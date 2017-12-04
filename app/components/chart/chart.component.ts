75faf9fb9d4df8d4cfbb63317fc95209ced9a2a1
lkh-PC\lkh <jaura7222@gmail.com>
2017-12-04 10:43:21 +0900

    12/04

--- app/components/chart/chart.component.ts
+++ app/components/chart/chart.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { ProductService} from './services/product.service';
 
 @Component({
 	selector : 'chart-page',
 	templateUrl : 'app/components/chart/chart.component.html'
 })
 export default class ChartComponent {
-	
+
+	product : Product;
+
 
 }
